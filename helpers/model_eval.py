daee9c0747314876b23699c0143fbfc097c0d5a2
Moritz Wilksch <moritzwilksch@gmail.com>
2021-01-25 15:09:45 +0100

    first catboost model

---
+++ helpers/model_eval.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import matplotlib.ticker as mtick
+
+
+
+def eval_model(ytrue, preds) -> pd.DataFrame:
+    evaldf = pd.DataFrame({'real': ytrue, 'pred': preds, 'ape': (preds-ytrue)/ytrue})
+    sns.set('notebook', font_scale=2)
+    sns.set_style('ticks')
+    fig, ax = plt.subplots(figsize=(15, 5))
+    sns.kdeplot(evaldf.ape * 100, color='blue', fill='red', ax=ax)
+    #sns.rugplot(evaldf.ape * 100, ax=ax, color='r')
+    pct_lower = np.percentile(evaldf.ape, 5) * 100
+    pct_upper = np.percentile(evaldf.ape, 95) * 100
+    ax.axvline(evaldf.ape.min()*100, color='r', ls='--')
+    ax.axvline(evaldf.ape.max()*100, color='g', ls='--')
+    ax.axvline(pct_lower, color='k', ls='--')
+    ax.axvline(pct_upper, color='k', ls='--')
+    ax.set_axis_below = True
+    ax.axvspan(pct_lower, pct_upper, color='k', alpha=0.1)
+    ax.axvspan(evaldf.ape.min()*100, pct_lower, color='red', alpha=0.1)
+    ax.axvspan(pct_upper, evaldf.ape.max()*100, color='green', alpha=0.1)
+    ax.xaxis.set_major_formatter(mtick.PercentFormatter(decimals=0))
+    ax.set(yticks=[], xlabel=r"% Error of model")
+    sns.despine()
+    plt.show()
+    return evaldf
---
+++ modelling/catboost_model.py
@@ -0,0 +1,53 @@
+# %%
+import pathlib
+import sys
+sys.path.append(str(pathlib.Path('..').resolve()))
+
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+from helpers.model_eval import eval_model
+
+root_path = "../"
+
+df = pd.read_csv(root_path + 'data/ISListingsBerlinBrb.csv')
+#df = df.fillna('NA')
+# %%
+from catboost import CatBoostRegressor
+
+cat_cols = ['postcode', 'quarter', 'city', 'balcony', 'barrier_free', 'builtin_kitchen', 'cellar', 'garden',
+            'guest_toilet', 'lift', 'street', 'energy_certificate', 'energy_efficiency']
+
+X = df.drop('title tags publish_date price creation housenr listing_id'.split(), axis=1)
+y = df.price/(df.living_space+1)
+
+add_no_cat = 'barrier_free cellar energy_certificate energy_efficiency guest_toilet'.split()
+X[add_no_cat] = X[add_no_cat].fillna('NO')
+X[cat_cols] = X[cat_cols].fillna('NA')
+numcols = X.loc[:, ~X.columns.isin(cat_cols)].columns
+
+X[cat_cols] = X[cat_cols].astype('category')
+xtrain, xval, ytrain, yval = train_test_split(X, y, random_state=42)
+
+# %%
+cbr = CatBoostRegressor(learning_rate=0.1, iterations=2000, cat_features=cat_cols, loss_function='MAPE', verbose=50)
+cbr.fit(xtrain, ytrain, eval_set=(xval, yval))
+
+# %%
+preds = cbr.predict(xval)
+
+# %%
+evaldf = eval_model(yval, cbr.predict(xval))
+
+#%%
+head25 = evaldf.sort_values('ape').head(25).index
+pd.concat((xval.loc[head25], yval.loc[head25]), axis=1)
+
+#%%
+# Underpriced:
+undervalued = evaldf.sort_values('ape').join([df, yval*xval.living_space]).rename({0: 'price'}, axis=1).drop(['tags', 'creation', 'publish_date', 'energy_certificate'], axis=1)
+undervalued.tail(25)
+
