a3a1d86b6cdc1c0ca90a03490950e0aaf827c310
Moritz Wilksch <moritzwilksch@gmail.com>
2022-02-03 16:58:36 +0100

    some eda

--- exploration/eda.py
+++ exploration/eda.py
@@ -1,13 +1,28 @@
 #%%
 import polars as pl
-
-df = pl.read_parquet("data/cleaned.parquet")
+import pandas as pd
+# df = pl.read_parquet("data/cleaned.parquet")
+df = pd.read_parquet("data/cleaned.parquet")
 
 #%%
-df.head().to_pandas()
+df.head()
 
+#%%
+# df = df.with_columns([
+#     pl.col("created_date").str.strptime(pl.Datetime, fmt="%Y-%m-%dT%H:%M:%S.%fZ"),
+#     pl.col("archived_date").str.strptime(pl.Datetime, fmt="%Y-%m-%dT%H:%M:%S.%fZ"),
+#     ])
+
+#%%
+df = df.assign(
+    created_date=df.created_date.astype("datetime64"),
+    archived_date=df.archived_date.astype("datetime64"),
+    duration=lambda d: (d["archived_date"]  -d["created_date"])
+)
 #%%
 import seaborn as sns
 import matplotlib.pyplot as plt
+sns.displot(df["duration"].dt.days, bins=50)
 
-sns.countplot(x=df.commercializationType.to_numpy())
+#%%
+df.duration.isnull().mean()
